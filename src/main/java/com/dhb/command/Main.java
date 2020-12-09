875677424b7cafae98ef324d95e6d6b9e3b105f3
haibo.duan <dhaibo1986@sohu.com>
2020-12-09 20:26:57 +0800

    增加责任链对do和undo进行处理

--- src/main/java/com/dhb/command/Main.java
+++ src/main/java/com/dhb/command/Main.java
@@ -1,5 +1,8 @@
 package com.dhb.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -19,5 +22,19 @@ public class Main {
 
 		System.out.println(c.msg);
 
+		List<Command> commands = new ArrayList<>();
+		commands.add(new InsertCommand(c));
+		commands.add(new CopyCommand(c));
+		commands.add(new DeleteCommand(c));
+
+		for(Command command : commands) {
+			command.doit();
+		}
+		System.out.println(c.msg);
+
+		for(int i=commands.size()-1;i>=0;i--){
+			commands.get(i).undo();
+		}
+		System.out.println(c.msg);
 	}
 }
